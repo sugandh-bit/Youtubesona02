66371709b3c3b8814776c04d3687909bef84c305
sugandh-bit <mrsugandh32@gmail.com>
2025-09-03 10:36:04 +0530

    Update userbot.py

--- SaregamaMusic/core/userbot.py
+++ SaregamaMusic/core/userbot.py
@@ -52,7 +52,7 @@ class Userbot(Client):
             await self.one.start()
             try:
                 await self.one.join_chat("MusiclineTechTeam")
-                await self.one.join_chat("MusiclineTechTeam")
+                await self.one.join_chat("Update_Jiosaavan")
             except:
                 pass
             assistants.append(1)
@@ -73,7 +73,7 @@ class Userbot(Client):
             await self.two.start()
             try:
                 await self.one.join_chat("MusiclineTechTeam")
-                await self.one.join_chat("MusiclineTechTeam")
+                await self.one.join_chat("Update_Jiosaavan")
             except:
                 pass
             assistants.append(2)
@@ -94,7 +94,7 @@ class Userbot(Client):
             await self.three.start()
             try:
                 await self.one.join_chat("MusiclineTechTeam")
-                await self.one.join_chat("MusiclineTechTeam")
+                await self.one.join_chat("Update_Jiosaavan")
             except:
                 pass
             assistants.append(3)
@@ -115,7 +115,7 @@ class Userbot(Client):
             await self.four.start()
             try:
                 await self.one.join_chat("MusiclineTechTeam")
-                await self.one.join_chat("MusiclineTechTeam")
+                await self.one.join_chat("Update_Jiosaavan")
             except:
                 pass
             assistants.append(4)
@@ -136,7 +136,7 @@ class Userbot(Client):
             await self.five.start()
             try:
                 await self.one.join_chat("MusiclineTechTeam")
-                await self.one.join_chat("MusiclineTechTeam")
+                await self.one.join_chat("Update_Jiosaavan")
             except:
                 pass
             assistants.append(5)
